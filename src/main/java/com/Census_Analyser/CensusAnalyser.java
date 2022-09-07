80a6a64257adf9b21955824499fab1a3193a2f0c
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-09-07 08:02:26 +0530

    Added: Welcome to Census Analyser Program

---
+++ src/main/java/com/Census_Analyser/CensusAnalyser.java
@@ -0,0 +1,10 @@
+package com.Census_Analyser;
+
+public class CensusAnalyser {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Census Analyser Program! ");
+		
+	}
+
+}
